964c1c8dce5e7cb16a1cde0accd90817ec003531
Jake Dluhy <dluhy2@illinois.edu>
2014-05-20 22:49:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IrisWebsite::Application.config.secret_key_base = '5a6abdb879eeb162e96e8b31dcda7c3254b23870ab405fe3a5d24c3624b168880931343f58d3257c61e542b4f6e4415b26976bfdbdea261b60bc4f8ce99f7bd0'
