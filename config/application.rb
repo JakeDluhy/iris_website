e97b560f13e0d51e8839d051624ab0a739053662
Jake Dluhy <dluhy2@illinois.edu>
2014-07-19 17:23:47 -0500

    Styling for heroku

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module IrisWebsite
     config.autoload_paths << Rails.root.join('lib')
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.serve_static_assets = true
   end
 end
